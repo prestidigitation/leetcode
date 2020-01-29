8393327504ca1dff7958c4e3fb9b3b6f76df2ae6
Alexander Rowland <rowlanda@protonmail.com>
2020-01-28 23:59:31 -0500

    js solution for two sum

---
+++ two-sum.js
@@ -0,0 +1,12 @@
+const twoSum = function(nums, target) {
+    let hash = {};
+    for (let i = 0; i < nums.length; i++) {
+        hash[nums[i]] = i;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (hash.hasOwnProperty(complement) && hash[complement] !== i) {
+            return [i, hash[complement]];
+        }
+    }
+};
